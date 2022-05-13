48b380b60c85fbb6539b7ceb9c0ca7da06d92e4d
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-05-13 15:47:12 +1000

    (wip) added first inquirer prompt (not working)

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+// -----------------------------
+
 // import dotenv to hide credentials
 require('dotenv').config();
+
 // import mysql2
-const mysql = require("mysql2");
+const mysql = require('mysql2');
+// import console.table
 const cTable = require('console.table');
+// import inquirer
+const inquirer = require('inquirer');
 
 // create the db connection using dotenv
 const db = mysql.createConnection(
@@ -15,13 +21,93 @@ const db = mysql.createConnection(
     console.log(`Connected to the ${process.env.DB_NAME} database`)
 );
 
-const init = () => {
-    const viewDepartmentTable = `SELECT * from department`;
+// -----------------------------
 
-    db.query(viewDepartmentTable, function(err, result) {
-        console.log(result);
-        console.table(result);
-    })
+
+const chooseWhatToDo = () => {
+    return inquirer
+        .prompt(
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments", "View all roles",
+                "View all employees",
+                new inquirer.Separator(),
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                new inquirer.Separator(),
+                "Update an employee's role"
+            ]
+        })
+        .then(answers => {
+            switch (answers.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addNewDepartment();
+                    break;
+                case "Add a role":
+                    addNewRole();
+                    break;
+                case "Add an employee":
+                    addNewEmployee();
+                    break;
+                case "Update an employee's role":
+                    updateEmployeeRole();
+                    break;
+                default:
+                    break;
+            }
+        })
+        .catch(err => console.log(err));
+
 }
 
+const viewAllDepartments = () => {
+    const viewDepartmentTable = `SELECT * from department`;
+
+    db.query(viewDepartmentTable, (err, result) => {
+        // console.log(result);
+        // console.log("\n");
+        console.table(result);
+    });
+}
+
+// -----------------------------
+
+
+
+const exit = () => {
+    return init();
+}
+
+
+
+const init = () => {
+    chooseWhatToDo();
+}
+
+
 init();
+
+
+
+// const init = () => {
+//     const viewDepartmentTable = `SELECT * from department`;
+
+//     db.query(viewDepartmentTable, function(err, result) {
+//         // console.log(result);
+//         console.log("\n");
+//         console.table(result);
+//     })
+// }
