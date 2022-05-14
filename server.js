bbbe8a1e89bf5c43ac6465cdf05ba02f1d54e1df
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-05-14 16:17:07 +1000

    added view funcs for roles & employees; updated view func for depts

--- server.js
+++ server.js
@@ -75,12 +75,60 @@ const chooseWhatToDo = () => {
 }
 
 const viewAllDepartments = () => {
-    const viewDepartmentTable = `SELECT * from department`;
+    const viewDepartmentTable = `
+        SELECT 
+            department.id AS "Department ID",
+            department.department_name AS "Department Name"
+        FROM department;
+    `;
 
     db.query(viewDepartmentTable, (err, result) => {
         // console.log(result);
         // console.log("\n");
         console.table(`\nDepartments`, result);
+        showMenu();
+    });
+}
+
+const viewAllRoles = () => {
+    const viewRolesTable = `
+        SELECT     
+            role.id AS "Role ID",
+            role.title AS "Role Title",
+            department.department_name AS "Department",
+            role.salary AS "Salary"
+        FROM role
+        JOIN department
+            ON role.department_id = department.id;
+    `;
+
+    db.query(viewRolesTable, (err, result) => {
+        // console.log(result);
+        // console.log("\n");
+        console.table(`\nRoles`, result);
+        showMenu();
+    });
+}
+
+const viewAllEmployees = () => {
+    const viewEmployeesTable = `
+        SELECT 
+            e.id AS "ID",
+            CONCAT(e.first_name, " ", e.last_name) AS "Employee Name",
+            role.title AS "Role",
+            role.salary AS "Salary",
+            CONCAT(m.first_name, " ", m.last_name) AS "Manager"
+        FROM employee e
+        LEFT JOIN role
+            ON e.role_id = role.id
+        LEFT OUTER JOIN employee m
+            ON e.manager_id = m.id;
+    `;
+
+    db.query(viewEmployeesTable, (err, result) => {
+        // console.log(result);
+        // console.log("\n");
+        console.table(`\nEmployees`, result);
         showMenu();
     });
 }
